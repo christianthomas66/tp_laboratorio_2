64c5031e3d70bb08de897106dd05082e2328906b
christianthomas66 <thomassuarezgrecco98@gmail.com>
2021-05-09 21:58:55 -0300

    TP2

---
+++ SuarezGrecco.ChristianThomas.TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+               return ETamanio.Chico;
+            }
+        }
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CICLOMOTOR\n");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendFormat("\n---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
